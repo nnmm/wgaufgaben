802584a22092d47a5997769b32f363ca21791843
nnmm <nnmmgit@gmail.com>
2017-01-30 19:33:47 +0100

    Coronation page first steps

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -5,11 +5,19 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../ui/layouts/app-body.js';
 import '../ui/pages/overview-page.js';
 import '../ui/pages/roomie-show-page.js';
+import '../ui/pages/coronation-page.js';
 
 FlowRouter.route('/roomie/:_id', {
   name: 'Roomie.show',
   action() {
     BlazeLayout.render('App_body', { main: 'Roomie_show_page' });
+  },
+});
+
+FlowRouter.route('/coronation', {
+  name: 'Coronation.show',
+  action() {
+    BlazeLayout.render('App_body', { main: 'Coronation_page' });
   },
 });
 
